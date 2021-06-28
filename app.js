a86f321d15e63b63a33d03c1f10c8039adcafd6f
tigerish009 <tigerish@gmail.com>
2021-06-28 21:44:42 +0900

    Array.from(color).forEach(color => color.addEventListener(click, handleColorClick)

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const canvas = document.getElementById("jsCanvas");
 const ctx = canvas.getContext("2d");
 
+const colors = document.getElementsByClassName("jsColor");
+
 // CSS의 canvas사이즈 외에 물리적 canvas의 사이즈를 지정해주지 않으면 작동하지 않는다.
 canvas.width = 700;
 canvas.height = 700;
@@ -30,10 +32,11 @@ function onMouseMove(event){
     }
 }
 
-function onMouseDown(event){
-    painting = true;
-}
 
+function handleColorClick(event){
+    const color = event.target.style.backgroundColor;
+    ctx.strokeStyle = color;
+}
 
 
 
@@ -44,3 +47,7 @@ if(canvas) {
     canvas.addEventListener("mouseup", stopPainting);
     canvas.addEventListener("mouseleave", stopPainting);
 }
+
+// Array.from은 object로 부터 array를 생성함
+// forEach안의 color라는 단어들은 아무거나 사용가능. object를 대표해주는 단어일뿐
+Array.from(colors).forEach(color => color.addEventListener("click", handleColorClick))
